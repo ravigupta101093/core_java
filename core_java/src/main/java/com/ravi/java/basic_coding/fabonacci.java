026f5b18d699d633e034f96f7bcf69b0f875d352
RAVI <guptaravi101093@gmail.com>
2018-12-27 09:30:06 -0800

    first commit

---
+++ core_java/src/main/java/com/ravi/java/basic_coding/fabonacci.java
@@ -0,0 +1,23 @@
+package com.ravi.java.basic_coding;
+
+class Fabonacci
+{   
+	int a=0,b=1,c,n=8,i;
+	void getdata()
+	{
+	
+		System.out.print(a+" "+b);
+		for(i=2;i<n;i++)
+		{
+		c=a+b;
+		System.out.print(" "+c);
+		a=b;
+		b=c;
+		}
+	}
+	public static void main(String args[])
+	{
+		Fabonacci f1=new Fabonacci();
+		f1.getdata();
+	}
+}
